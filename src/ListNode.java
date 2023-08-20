3a545893e5e31090581c52ca0173d4d97d2ca2a9
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-20 13:18:02 +0400

    two more algorithm added

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,13 +1,35 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 class Main {
     public static void main(String[] args){
-        System.out.println(
-                findEvenIndex(new int[] {1,2,3,4,5,6})
-        );
+        System.out.println(camelCase("HelloWorld"));
+    }
+
+    // greed is good -> skiped!
+    // Break camelCase
+    public static String camelCase(String input) {
+//        if (Objects.equals(input, "")) return "";
+        return String.join(" ",input.split("(?=[A-Z])"));
+    }
+    // build a tower
+    public static String[] towerBuilder(int nFloors)
+    {
+        String[] tower = new String[nFloors];
+        int maxWidth = 2 * nFloors - 1;
+
+        for (int i = 0; i < nFloors; i++) {
+            int numStars = 2 * i + 1;
+            int numSpaces = (maxWidth - numStars) / 2;
+            String floor = " ".repeat(numSpaces) + "*".repeat(numStars) + " ".repeat(numSpaces);
+            tower[i] = floor;
+        }
+
+        return tower;
     }
     // Equal Sides of an array
     public static int findEvenIndex(int[] arr) {
+
         for (int i = 0;i < arr.length;i++) {
             int[] splitedFrist = Arrays.copyOfRange(arr,0,i);
             int[] splitedSecond = Arrays.copyOfRange(arr,i+1,arr.length);
